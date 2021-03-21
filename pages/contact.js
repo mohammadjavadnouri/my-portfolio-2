e3b4be3d8b70cbc3a81f55334da211f03c062cea
Your Name <>
2021-03-21 08:19:28 +0330

    add contact page

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import styles from "./contact.module.scss";
+import "normalize.css/normalize.css";
+
+const Contact = () => (
+  <div className={styles.container}>
+    <p>Tell: 0098-9397988559</p>
+    <p>Mail: nouri_mj@yahoo.com</p>
+    <div>
+      <img src="/Front-end-logo.png" alt="mark" />
+    </div>
+  </div>
+);
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { Button, ButtonGroup } from "@material-ui/core";
+
 import styles from "./index.module.scss";
 import "normalize.css/normalize.css";
 
@@ -6,14 +8,16 @@ const Index = () => (
   <div className={styles.container}>
     <div>
       <h1>MOHAMADJAVAD NOURI</h1>
-      <p>May be a Front-End Web Developer</p>
+      <p>May be a Web Developer</p>
     </div>
     <div>
       <ButtonGroup variant="text" aria-label="text primary button group">
         <Button>About</Button>
         <Button>Works</Button>
         <Button>Services</Button>
-        <Button>Contact</Button>
+        <Link href="/contact">
+          <Button>Contact</Button>
+        </Link>
       </ButtonGroup>
     </div>
     <div>
